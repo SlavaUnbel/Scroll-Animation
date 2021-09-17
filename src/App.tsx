ded7844fdfa2b0a38912c5d9c47f5af7812bed73
LevkovichV <slavlen1999@gmail.com>
2021-09-17 18:24:25 +0300

    created content and applyed styles to it

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,91 @@
 import React from 'react';
 
 const App: React.FC = () => {
-  return <div className="container"></div>;
+  return (
+    <div className="container">
+      <h1>Scroll to see the animation</h1>
+
+      <div className="box">
+        <h2>Content</h2>
+      </div>
+
+      <div className="box">
+        <h2>Content</h2>
+      </div>
+
+      <div className="box">
+        <h2>Content</h2>
+      </div>
+
+      <div className="box">
+        <h2>Content</h2>
+      </div>
+
+      <div className="box">
+        <h2>Content</h2>
+      </div>
+
+      <div className="box">
+        <h2>Content</h2>
+      </div>
+
+      <div className="box">
+        <h2>Content</h2>
+      </div>
+
+      <div className="box">
+        <h2>Content</h2>
+      </div>
+
+      <div className="box">
+        <h2>Content</h2>
+      </div>
+
+      <div className="box">
+        <h2>Content</h2>
+      </div>
+
+      <div className="box">
+        <h2>Content</h2>
+      </div>
+
+      <div className="box">
+        <h2>Content</h2>
+      </div>
+
+      <div className="box">
+        <h2>Content</h2>
+      </div>
+
+      <div className="box">
+        <h2>Content</h2>
+      </div>
+
+      <div className="box">
+        <h2>Content</h2>
+      </div>
+
+      <div className="box">
+        <h2>Content</h2>
+      </div>
+
+      <div className="box">
+        <h2>Content</h2>
+      </div>
+
+      <div className="box">
+        <h2>Content</h2>
+      </div>
+
+      <div className="box">
+        <h2>Content</h2>
+      </div>
+
+      <div className="box">
+        <h2>Content</h2>
+      </div>
+    </div>
+  );
 };
 
 export default App;
